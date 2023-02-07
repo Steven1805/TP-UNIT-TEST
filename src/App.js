02339e61b3f68147a62fcb570535d5faee40fac0
Steven1805 <hei.steven.2@gmail.com>
2023-02-07 23:07:08 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [todoItems, setTodoItems] = useState([
+  ]);
+
+  const [doneItems, setDoneItems] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleCheckboxChange = (id) => {
+    const updatedTodoItems = todoItems.map((item) => {
+      if (item.id === id) {
+        return { ...item, done: true };
+      }
+      return item;
+    });
+
+    setDoneItems([
+      ...doneItems,
+      ...updatedTodoItems.filter((item) => item.done),
+    ]);
+    setTodoItems(updatedTodoItems.filter((item) => !item.done));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodoItems([...todoItems, { id: Date.now(), text: newTodo, done: false }]);
+    setNewTodo("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          data-testid="todoinput"
+          type="text"
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.target.value)}
+        />
+        <button data-testid="todosubmit" className='add-todo' type="submit">Add Todo</button>
+      </form>
+      <div className='Todos'>
+      <div>
+      <h2>To-Do</h2>
+      <ul data-testid="todoitems" >
+        {todoItems.map((item) => (
+          <li key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.done}
+              onChange={() => handleCheckboxChange(item.id)}
+            />
+            {item.text}
+          </li>
+        ))}
+      </ul>
+      </div>
+      <div>
+      <h2>Done</h2>
+      <ul data-testid='doneitems'>
+        {doneItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+      </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+// Testing if when I click enter, the todo Item as a checkbox appears
+
+import React from 'react';
+import { render, fireEvent,within, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render a form with a text input and a submit button', () => {
+    render(<App />)
+    const input = screen.getByTestId("todoinput");
+    const submit = screen.getByTestId("todosubmit");
+    expect(input).toBeInTheDocument();
+    expect(submit).toBeInTheDocument();
+
+
+  });
+
+  it('should add a to-do item to the to-do list when the form is submitted', () => {
+    render(<App />)
+    const input = screen.getByTestId("todoinput");
+    const submit = screen.getByTestId("todosubmit");
+    const listTodo = screen.getByTestId("todoitems");
+    fireEvent.change(input, {target:{value: "submitted to do"}});
+    fireEvent.submit(submit);
+    const itemsAfterSubmit = within(listTodo).getAllByRole("listitem");
+    const lengthAfterSubmit = itemsAfterSubmit.length;
+    expect(lengthAfterSubmit).toBe(1)
+  });
+
+  it('should move a to-do item to the done list when the checkbox is checked', () => {
+    render(<App />)
+    const input = screen.getByTestId("todoinput");
+    const submit = screen.getByTestId("todosubmit");
+    const listTodo = screen.getByTestId("todoitems");
+    fireEvent.change(input, {target:{value: "submitted to do"}});
+    fireEvent.submit(submit);
+    const itemsAfterSubmit = within(listTodo).getAllByRole("listitem");
+    const lengthAfterSubmit = itemsAfterSubmit.length;
+    const item = screen.getByText("submitted to do");
+    const listItem = item.closest("listitem");
+    const checkbox = within(within(listTodo).getAllByRole("listitem")[0]).getAllByRole("checkbox")[0];
+    fireEvent.click(checkbox);
+    const done = screen.getByTestId("doneitems");
+    const doneAfterSubmit = within(done).getAllByRole("listitem");
+    const donelength = doneAfterSubmit.length;
+    expect(donelength).toBe(1)
+  });
 });
+
